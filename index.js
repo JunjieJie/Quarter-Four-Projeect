64f948a7eb8534556a7bf4fb2ef791cf5d545db7
plegaspi <39167788+plegaspi@users.noreply.github.com>
2020-04-03 13:29:08 -1000

    Resolved the disappearance of the player and  added player functionality
    The player no longer disappears and the "cars" will scroll along with it. The scroll behavior of the barriers, however, has not been fully fleshed out.

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
  */
 var w;
 var h;
-var player = new Character();
+var canvas;
+var player;
 var score;
 var vehicles = [];
+var moveUp;
 var hit = false;
 /*
  **
@@ -13,7 +15,8 @@ var hit = false;
 function setup() {
   w = window.innerWidth;
   h = window.innerHeight;
-  var canvas = createCanvas(w, h);
+  canvas = createCanvas(w, h);
+  player = new Player();
   //  Initialize Objects
   for (var i = 0; i < 3; i++) {
     vehicles.push(new Vehicle());
@@ -41,22 +44,28 @@ function draw() {
     window.alert("game over");
   }
   //move player down on the W key and up arrow key
-  if (keyIsDown(87) && player.y > 10) {
+  if (keyIsDown(87) && player.y - 0.1 * h > 0) {
     player.y -= player.speed;
+    for (var i = 0; i < vehicles.length; i++) {
+      vehicles[i].y += 0.1 * h;
+    }
     //distance needed to go to get point) {}
     document.getElementById("points").innerHTML++; //why just keep it "++"
   }
   //move player down on the S key and down arrow key
-  if (keyIsDown(83) && player.y < h - 60) {
+  if (keyIsDown(83) && player.y + 0.1 * h < h) {
     player.y += player.speed;
+    for (var i = 0; i < vehicles.length; i++) {
+      vehicles[i].y -= 0.1 * h;
+    }
     document.getElementById("points").innerHTML--;
   }
   //move player left on the A key and left arrow key
-  if (keyIsDown(65) && player.x > 10) {
+  if (keyIsDown(65) && player.x - 0.1 * h > 0) {
     player.x -= player.speed;
   }
   //move player right on the D key and the right arrow key
-  if (keyIsDown(68) && player.x < w - 60) {
+  if (keyIsDown(68) && player.x < w - 0.1 * h) {
     player.x += player.speed;
   }
 }
@@ -66,6 +75,7 @@ function draw() {
  */
 // Responsive Canvas
 function windowResized() {
+  console.log("hello");
   w = window.innerWidth;
   h = window.innerHeight;
   resizeCanvas(w, h);
--- objects.js
+++ objects.js
@@ -9,11 +9,11 @@ function Vehicle() {
   this.x = w + this.width;
   this.y = Math.floor(Math.random() * 10) * 0.1 * h;
   this.speed = 10; //I still don't understand how this speeds gonna work - JJ
-  this.show = function() {
+  this.show = function () {
     fill("yellow");
-    rect(this.x, this.y, 100, 50);
+    rect(this.x, this.y, this.width, this.height);
   };
-  this.update = function() {
+  this.update = function () {
     this.x -= this.speed;
     if (this.x < 0) {
       //checks if it goes off the screen and resets it if it does
@@ -24,13 +24,13 @@ function Vehicle() {
   };
 }
 
-function Character() {
+function Player() {
   //  Player Properties
   this.dimensions = [];
   this.x = w / 2;
-  this.y = h - 0.1 * h;
+  this.y = h - 0.5 * 0.1 * h;
   this.speed = 0.1 * h;
-  this.show = function() {
+  this.show = function () {
     fill("orange"); //so u cant make this a pic?
     circle(this.x, this.y, 0.1 * h);
     console.log("your dad");
